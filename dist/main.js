7aebc749f41a27848f73913ffd55f76c0605ec39
mongi2022 <mongithabet@tekniva.net>
2023-02-17 10:20:40 +0100

    up

--- dist/main.js
+++ dist/main.js
@@ -8,7 +8,7 @@ async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.use(cookieParser());
     app.enableCors({
-        origin: 'http://localhost:4200',
+        origin: '*',
         credentials: true
     });
     const options = new swagger_1.DocumentBuilder()
